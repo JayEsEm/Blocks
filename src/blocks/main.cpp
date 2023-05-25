c3e5635459245c233b94687a903ccc7c2cc97305
Jorge San Emeterio <jorge.s.emeterio@gmail.com>
2023-05-25 11:12:57 +0200

    Adding dependencies

--- src/blocks/main.cpp
+++ src/blocks/main.cpp
@@ -1,5 +1,20 @@
 #include "blocks/main.hpp"
 
+#include <raylib-cpp.hpp>
+
 void blocks::main()
 {
+    raylib::Window window(800, 450, "Blocks!");
+    {
+        window.SetTargetFPS(60);
+
+        while (window.ShouldClose() == false)
+        {
+            window.BeginDrawing();
+            {
+                window.ClearBackground(raylib::Color::RayWhite());
+            }
+            window.EndDrawing();
+        }
+    }
 }
