d8b8bb0f8cdb194b60ed819f1ed86cc0de9c03c3
Jorge San Emeterio <jorge.s.emeterio@gmail.com>
2023-05-29 14:44:20 +0200

    Allow conversion from color to raylib format.

--- inc/bl/color.hpp
+++ inc/bl/color.hpp
@@ -1,6 +1,8 @@
 #ifndef A28D31EE_674E_4469_8C3F_2612530A8472
 #define A28D31EE_674E_4469_8C3F_2612530A8472
 
+#include <raylib.hpp>
+
 namespace bl
 {
     struct color
@@ -10,6 +12,12 @@ namespace bl
             unsigned char g;
             unsigned char b;
             unsigned char a;
+
+        public:
+            raylib::Color to_raylib()
+            {
+                return raylib::Color(r, g, b, a);
+            }
     };
 }
 
