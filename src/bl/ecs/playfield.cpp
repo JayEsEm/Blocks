24fa164277ece3ab041f248acab28b531b977ba4
Jorge San Emeterio <jorge.s.emeterio@gmail.com>
2023-06-01 11:45:43 +0200

    Starting once more.

--- src/bl/ecs/playfield.cpp
+++
@@ -1,7 +0,0 @@
-#include "bl/ecs/playfield.hpp"
-
-bl::ecs::playfield::playfield(unsigned int w, unsigned int h)
-    : cells(w * h)
-{
-    std::fill(cells.begin(), cells.end(), bl::ecs::playfield::cell::n);
-}
